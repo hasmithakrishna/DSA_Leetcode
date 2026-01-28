59d6bebfea0120ffdd2370fec92791392c3a9eb3
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-01-28 15:29:14 -0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
+    vector<int> Arr;
     int fib(int n) {
-        if(n<=1)
-        return n;
-        else{
-            return fib(n-1) + fib(n-2);
+        if(Arr.empty()){
+            Arr.assign(n+1,-1);
         }
+        if(n <= 1){
+            Arr[n] = n;
+            return n;
+        }
+        if(Arr[n-2] == -1){
+            Arr[n-2] = fib(n-2);
+        }
+        if(Arr[n-1] == -1){
+            Arr[n-1] = fib(n-1);
+        }
+        return Arr[n-2] + Arr[n-1];
+
+        
     }
 };
