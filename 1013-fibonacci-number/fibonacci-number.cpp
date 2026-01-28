7b3ae26bc2e7a82a9ec93fcb604007658edc29dc
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-01-28 15:32:10 -0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> Arr;
+    //Can be done easily with recursion fib(n-2) + fib(n-1) but it makes repeated function calls with same values which leads to excessive recursion. So we use a concept called memorization. First we initialize an array with -1. Update that array.
     int fib(int n) {
         if(Arr.empty()){
             Arr.assign(n+1,-1);
