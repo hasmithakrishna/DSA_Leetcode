62401c341536e6a6a8854d6e29806a90117ebc05
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-02-02 15:32:43 -0500

    Time: 1 ms (22.07%) | Memory: 22.2 MB (9.15%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //We create a new array to note frequencies from 0 to n, then return the index whose frequency is zero.
-        
+        //we create a new array and 
         int n= nums.size();
         vector<int> arrayfreq(n+1,0);
         for(int i=0; i<n; i++){
