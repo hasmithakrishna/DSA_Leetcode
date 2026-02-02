0a80082a0cf5f418799d63d761a1ef55b7c5857f
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-02-02 15:54:21 -0500

    Time: 11 ms (11.67%) | Memory: 28.1 MB (6.59%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n= nums.size();
-        vector<int> arrayfreq(n+1,0);
-        for(int i=0; i<n; i++){
-            arrayfreq[nums[i]]++;
+        int n = nums.size();
+        unordered_map<int,int> freq;
+        for (int i = 0; i < n; i++) {
+            freq[nums[i]]++;
         }
-        for(int i=0; i<=n; i++){
-            if(arrayfreq[i]==0)
-            return i;
+        for (int i = 0; i <= n; i++) {
+            if (freq[i] == 0)
+                return i;
         }
         return -1;
-        
-        
     }
 };
