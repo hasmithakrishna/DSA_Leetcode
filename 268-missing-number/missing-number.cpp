4b23582e274a656b8b07ab83e1a69e49e16c4848
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-02-02 15:32:50 -0500

    Time: 0 ms (100.00%) | Memory: 22.4 MB (8.07%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //we create a new array and 
         int n= nums.size();
         vector<int> arrayfreq(n+1,0);
         for(int i=0; i<n; i++){
