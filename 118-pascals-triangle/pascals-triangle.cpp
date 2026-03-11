6a3fb5360b8d11eb681f220158c2b594c2d6ce60
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-03-11 14:55:11 -0400

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.67%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -7,12 +7,12 @@ public:
             matrix[i][0]=1;
             matrix[i][i]=1;
         }
-        for(int i=2;i<numRows;i++){
+        for(int i=2; i<numRows; i++){
             for(int j=1; j<i; j++){
-                matrix[i][j] = matrix[i-1][j-1]+matrix[i-1][j]; 
+                matrix[i][j] = matrix[i-1][j-1]+matrix[i-1][j];
             }
         }
-        
         return matrix;
+        
     }
 };
