9eb16aa2a11a6f6fb89fd84afcf5960de44973d7
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-03-11 14:47:51 -0400

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.67%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> matrix(numRows);
+        for(int i=0; i<numRows; i++){
+            matrix[i].resize(i+1);
+            matrix[i][0]=1;
+            matrix[i][i]=1;
+        }
+        for(int i=2;i<numRows;i++){
+            for(int j=1; j<i; j++){
+                matrix[i][j] = matrix[i-1][j-1]+matrix[i-1][j]; 
+            }
+        }
+        
+        return matrix;
+    }
+};
