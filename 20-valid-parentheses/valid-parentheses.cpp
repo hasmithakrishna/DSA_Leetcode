6d86cdab4bf2e1c2c91a94cb7cf45465bdec4cbc
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-03-26 18:25:30 -0400

    Time: 3 ms (9.81%) | Memory: 9.1 MB (20.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = int(s.size());
-        char *Arr = new char[n];
-        int top=-1;
-        for(int i=0; i<n; i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                if(top==n-1) return false;
-                top++;
-                Arr[top] = s[i];
+        stack<char> st;
+        for(int i=0; i< s.size(); i++){
+            if(s[i]=='(' ||s[i]=='[' ||s[i]=='{' ){
+                st.push(s[i]);
             }
-            if(s[i]==')' || s[i]==']' || s[i]=='}'){
-                if(top==-1) return false;
-                char open = Arr[top];
-                top--;
-                if((open =='(' && s[i] !=')') || 
-                (open =='[' && s[i] !=']') || 
-                (open =='{' && s[i] !='}'))
-                return false; 
-                
+            else{
+                if(st.empty()) return false;
+                if((s[i]==')' && st.top() == '(') || (s[i]==']' && st.top() == '[') || (s[i]=='}' && st.top()=='{'))
+                st.pop();
+                else return false;
             }
         }
-        return (top==-1); 
+        return st.empty();
+        
     }
 };
