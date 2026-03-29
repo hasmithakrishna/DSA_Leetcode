e5ec67466ced20cf7ceeb869988fecbec8b7ece7
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-03-29 15:42:51 -0400

    Time: 0 ms (100.00%) | Memory: 12.7 MB (58.86%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -5,17 +5,11 @@ public:
         stack<int> st;
         vector<int> ans;
         for(int i = nums2.size() - 1; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums2[i]) st.pop();
             if(st.empty()) mp[nums2[i]] = -1;
-            else {
-                if(st.top() > nums2[i]) mp[nums2[i]] = st.top();
-                else {
-                    while(!st.empty() && st.top() <= nums2[i]) st.pop();
-                    if(st.empty()) mp[nums2[i]] = -1;
-                    else mp[nums2[i]] = st.top();
-                    }
-                    }
-                    st.push(nums2[i]);
-                    }
+            else mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
+            }
         for(int i=0; i<nums1.size(); i++){
             ans.push_back(mp[nums1[i]]);
         }
