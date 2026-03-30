8ad9344a9e3193ed56a35752010112f05d221207
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-03-30 11:37:30 -0400

    Time: 51 ms (8.76%) | Memory: 96.3 MB (5.05%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -6,7 +6,9 @@ public:
         stack<pair<int,int>> st1;
         int maximum = 0;
         for(int i=0; i<heights.size();i++){
-            while(!st1.empty() && st1.top().first >= heights[i]) st1.pop();
+            while(!st1.empty() && st1.top().first >= heights[i]){
+                st1.pop();
+                }
                 if(st1.empty()) Left.push_back(-1);
                 else Left.push_back(st1.top().second);
             st1.push({heights[i],i});
@@ -15,7 +17,9 @@ public:
             st1.pop();
         }
         for(int i= heights.size()-1; i>=0; i--){
-            while(!st1.empty() && st1.top().first >= heights[i]) st1.pop();
+            while(!st1.empty() && st1.top().first >= heights[i]){
+                st1.pop();
+            }
             if(st1.empty()) Right.push_back(heights.size());
             else Right.push_back(st1.top().second);
 
