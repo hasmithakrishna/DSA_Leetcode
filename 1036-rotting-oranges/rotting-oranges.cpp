aa396ed62df0ab0d02dac6a13ee5342cbcb2ab9f
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-04-05 23:12:44 -0400

    Time: 0 ms (100.00%) | Memory: 16.6 MB (97.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,29 +4,25 @@ class Solution {
         int n = grid[0].size();
         int count = -1;
         int delrow[] = {-1,0,+1,0};
-        int delcol[] = {0,-1,0,+1};
+        int delcol[] = {0,+1,0,-1};
         while(!q.empty()){
             int size = q.size();
             count++;
-        for(int i=0; i<size; i++){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop(); 
-            for(int j=0; j<4; j++){
-                int newrow = row + delrow[j];
-                int newcol = col + delcol[j];
-                if(newrow>=0 && newrow<m && newcol >=0 && newcol <n && grid[newrow][newcol] == 1){
-                    grid[newrow][newcol] = 2;
-                    q.push({newrow, newcol});
+            for(int i=0; i< size; i++){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                for(int j=0; j<4; j++){
+                    int newrow = row+delrow[j];
+                    int newcol = col+delcol[j];
+                        if(newrow>=0 && newrow<m && newcol>=0 && newcol <n && grid[newrow][newcol] == 1){
+                            grid[newrow][newcol] = 2;
+                            q.push({newrow, newcol});
+                }
                 }
             }
-
-
-        }
         }
         return max(count,0);
-
-
 
     }
 public:
@@ -47,8 +43,6 @@ public:
                 if(grid[i][j] == 1) return -1;
             }
         }
-
         return ans;
     }
-
 };
