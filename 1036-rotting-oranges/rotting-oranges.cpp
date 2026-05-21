85354e891e798825919c4560b545ee628f274461
hasmithakrishna <75234540+hasmithakrishna@users.noreply.github.com>
2026-05-21 08:19:33 -0400

    Time: 4 ms (15.31%) | Memory: 17.1 MB (28.21%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int count = 0;
         int m = grid.size();
         int n = grid[0].size();
         queue<pair<int,int>> q;
@@ -10,39 +11,37 @@ public:
                 if(grid[i][j] == 2){
                     q.push({i,j});
                     vis[i][j] = 1;
+
                 }
             }
         }
-        int count = -1;
         int delrow[] = {-1,0,+1,0};
         int delcol[] = {0,+1,0,-1};
         while(!q.empty()){
-            int x = q.size();
-            count++;
-            for(int i=0; i<x; i++){
-                int row = q.front().first;
-                int col = q.front().second;
-                q.pop();
-                for(int i=0; i<4; i++){
-                    int newrow = row+ delrow[i];
-                    int newcol = col+delcol[i];
-                    if(newrow>=0 && newrow<m && newcol >=0 && newcol <n && vis[newrow][newcol]==0 && grid[newrow][newcol] == 1){
-                        grid[newrow][newcol] = 2;
-                        vis[newrow][newcol] = 1;
-                        q.push({newrow, newcol});
-                    }
+            int sq = q.size();
+            bool spread= false;
+            for(int j=0; j<sq; j++){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            for(int i=0; i<4; i++){
+                int newrow = row + delrow[i];
+                int newcol = col + delcol[i];
+                if( newrow >=0 && newcol >=0 && newrow < m && newcol <n && grid[newrow][newcol]==1 && vis[newrow][newcol] == 0){
+                    q.push({newrow,newcol});
+                    vis[newrow][newcol] = 1;
+                    spread = true;
                 }
-
             }
-
+            }
+            if(spread) count++;
         }
-
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == 1) return -1;
+                if(grid[i][j] == 1 && vis[i][j] == 0) return -1;
             }
         }
-        return max(count,0);
-
+        return count;
+        
     }
 };
